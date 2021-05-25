d17e326cd41473872b40b3cabc696a3b082a5241
topcheyalex <topcheyalex@gmail.com>
2021-05-25 17:17:03 +0300

    Commit 1_1

--- Program.cs
+++ Program.cs
@@ -11,6 +11,7 @@ namespace ConsoleApp17
         static void Main(string[] args)
         {
             Console.WriteLine("Instruction 1");
+            Console.WriteLine("Instruction 2");
         }
     }
 }
